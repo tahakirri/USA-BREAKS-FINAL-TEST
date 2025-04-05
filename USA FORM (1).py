034afb3fad653225f55f218c08816bcafd9fd9eb
tahakirri <taha.kirri@gmail.com>
2025-04-05 00:32:53 +0000

    Update USA FORM (1).py

--- USA FORM (1).py
+++ USA FORM (1).py
@@ -13,7 +13,7 @@ from threading import Thread
 # --------------------------
 # Database Functions
 # --------------------------
-  def get_db_connection():
+def get_db_connection():
     """Create and return a database connection."""
        os.makedirs("data", exist_ok=True)
         conn = None
